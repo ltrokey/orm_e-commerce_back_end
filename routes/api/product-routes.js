fe4c99ca742e421312e3df17722c50e774a75170
Lacey Trokey <trokeyln@gmail.com>
2023-11-13 13:08:19 -0700

    added GET routes for products and tested

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,18 +1,32 @@
 const router = require('express').Router();
 const { Product, Category, Tag, ProductTag } = require('../../models');
 
-// The `/api/products` endpoint
-
-// get all products
-router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+router.get('/', async (req, res) => {
+  try{
+    const products = await Product.findAll({
+      include: [
+        {model: Category},
+        {model: Tag},
+      ]
+    })
+    return res.json(products)
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
-// get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+router.get('/:id', async (req, res) => {
+  try{
+    const productData = await Product.findByPk(req.params.id, {
+      include: [
+        {model: Category},
+        {model: Tag},
+      ]
+    })
+    return res.json(productData)
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 // create new product
